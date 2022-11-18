ef0a1cbb7cd52834414f89ad23e5dc8d62d4de99
Prashant Chavan <pchavan@ups.com>
2022-11-18 16:01:05 +0100

    initial setup - step 1

--- src/main/resources/static/js/welcome.js
+++ src/main/resources/static/js/welcome.js
@@ -1,3 +1,11 @@
-function getSomething(){
+require('jquery')
+
+function getSomething() {
     return "if you have called getSomething you will get something....";
 }
+
+$(function() {
+    console.log( "welcome to page !!!" );
+    console.log(getSomething());
+});
+

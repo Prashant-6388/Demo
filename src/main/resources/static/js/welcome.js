1f681b4c14a4963e1b628dd3b929304b90a3f006
Prashant <prashant.6388@gmail.com>
2022-11-20 11:22:58 +0100

    Binding function to element

--- src/main/resources/static/js/welcome.js
+++ src/main/resources/static/js/welcome.js
@@ -7,12 +7,13 @@ function getSomething() {
 $(function() {
     console.log( "welcome to page !!!" );
     console.log(getSomething());
+
+    $("#btn1").on("click", function(){
+      console.log("btn1 clicked.....");
+    });
 });
 
 export function testStartFunction() {
   console.log("test function called");
 }
 
-$("#btn1").on("click", function(){
-  console.log("btn1 clicked.....");
-});

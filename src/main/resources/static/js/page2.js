0a5a644e2b8880af43dd9449f009aafb90238e82
Prashant <prashant.6388@gmail.com>
2022-11-20 21:18:31 +0100

    Getting setting DOM element using JQuery

--- src/main/resources/static/js/page2.js
+++ src/main/resources/static/js/page2.js
@@ -8,5 +8,15 @@ import 'jquery-ui/ui/widgets/datepicker';
 
 $(function(){
   console.log("page2 is called");
-  $( "#datepicker" ).datepicker();
+  $("#mydatepicker").datepicker();
+
+  $("#btn3").on("click", function() {
+    console.log("btn3 clicked.....");
+    console.log("date selected jq: " + $('#mydatepicker').attr('value'));
+    console.log("date selected jq using dom element: " + $('#mydatepicker')[0].value);
+    //to understand behaviour look at  https://stackoverflow.com/questions/74510980/get-element-using-jquery-with-webpack/74511005
+    console.log("date selected jq using val() method: " + $('#mydatepicker').val());
+    console.log("date selected js: " + document.getElementById("mydatepicker").value);
+    $('#mydatepicker').val("01/01/1900");
+  });
 });
--- src/main/resources/templates/page2.jsp
+++ src/main/resources/templates/page2.jsp
@@ -1,13 +1,14 @@
 <html>
     <head>
         <title>Home Page</title>
-        <script language="JavaScript" type="text/javascript" src="../js/welcome.js"></script>
         <script language="JavaScript" type="text/javascript" src="../js/page2.js"></script>
         <link rel="stylesheet" type="text/css" href="../css/welcome.css" />
     </head>
     <body>
         <h1>Hello !</h1>
         <p>Welcome to <span th:text="${pageName}"> </span></p>
-        <p>Date: <input type="text" id="datepicker"></p>
-    </body>
+        <p>Date: <input type="text" id="mydatepicker"></p>
+        <button id = "btn2" onclick="btnClick('test');">Page 2 Click Me - 2</button>
+        <button id = "btn3">Page 2 Click Me - Btn1</button>
+     </body>
 </html>
